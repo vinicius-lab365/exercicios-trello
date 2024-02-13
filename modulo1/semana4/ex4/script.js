66046ad5bd10c8f4ca2079c4b950745514694873
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-02-13 17:32:58 -0300

    Finalizado exercício 4

---
+++ modulo1/semana4/ex4/script.js
@@ -0,0 +1,7 @@
+// Crie uma função que receba dois números como parâmetros e retorne sua soma no console.log.
+
+function sum (numbe1, number2) {
+    return numbe1 + number2;
+}
+
+console.log(sum(3, 2));
