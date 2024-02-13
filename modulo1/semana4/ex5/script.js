0c44c2f1ae86fe38f63921656c51cd44bb4b82eb
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-02-13 17:40:41 -0300

    Finalizado exercício 5

---
+++ modulo1/semana4/ex5/script.js
@@ -0,0 +1,14 @@
+// Utilize uma estrutura condicional (if,else) para verificar se um número é positivo, negativo ou zero. Exiba os valores no console.log()
+
+let number = -3;
+let answer = "";
+
+if (number > 0) {
+    answer = "Positivo"
+} else if (number < 0) {
+    answer = "Negativo";
+} else {
+    answer = "Zero";
+}
+
+console.log(answer);
