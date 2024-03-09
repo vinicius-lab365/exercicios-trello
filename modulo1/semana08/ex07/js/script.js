dba6b4df86ccef6ad19a25646f8a544529021255
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-03-09 10:28:15 -0300

    Finalizado ex07

---
+++ modulo1/semana08/ex07/js/script.js
@@ -0,0 +1,22 @@
+const elements = document.getElementsByClassName('item-churrasco');
+
+const itensChurrasco = [
+    'Cerveja',
+    'Carvão',
+    'Alcool',
+    'Fósforo',
+    'Carne',
+    'Linguiça',
+    'Pão de alho',
+    'Farófa',
+    'Coração de frango'
+]
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].textContent = `${itensChurrasco[i]}`
+}
+
+const titulo = document.querySelector('h1');
+titulo.classList.add('fs-36');
+
+titulo.classList.remove('fs-36');
