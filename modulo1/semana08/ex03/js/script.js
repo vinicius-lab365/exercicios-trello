63e4b6ddb3c0da56b5ff9fbace221ee1f135a34f
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-03-09 09:43:38 -0300

    Finalizado ex03

---
+++ modulo1/semana08/ex03/js/script.js
@@ -0,0 +1,23 @@
+const elements = document.getElementsByClassName('item-churrasco');
+
+const itensChurrasco = [
+    'Cerveja',
+    'Carvão',
+    'Alcool',
+    'Fósforo',
+    'Carne',
+    'Linguiça',
+    'Pão de alho',
+    'Farófa',
+    'Coração de frango'
+]
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].textContent = `${itensChurrasco[i]}`
+}
+
+const listaItensChurrasco = document.getElementById('lista-churrasco')
+const meuElemento = document.createElement('li');
+let texto = document.createTextNode('Sal grosso');
+meuElemento.appendChild(texto);
+listaItensChurrasco.appendChild(meuElemento);
